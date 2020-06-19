46e110614379521e8dd8ffa8a1c59967bbb9c729
gagaman182 <csage182*>
2020-06-19 19:55:07 +0700

    insert bar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,67 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <v-app id="inspire">
+      <v-layout row justify-center>
+        <v-toolbar
+          app
+          dark
+          color="blue-grey darken-1"
+          class="hidden-xs-and-down"
+        >
+          <v-toolbar-title
+            >แบบฟอร์มประเมินตัวเองเฝ้าระวังการติดเชื้อไวรัสโคโรน่า
+            2019</v-toolbar-title
+          >
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn
+              v-for="item in nav"
+              :key="item.icon"
+              :to="item.link"
+              :title="item.title"
+              flat
+              >{{ item.text }}</v-btn
+            >
+          </v-toolbar-items>
+        </v-toolbar>
+        <v-container fluid grid-list-xl>
+          <v-layout row justify-space-between>
+            <v-flex xs2> </v-flex>
+            <v-flex xs2>
+              <router-view />
+            </v-flex>
+            <v-flex xs2> </v-flex>
+          </v-layout>
+        </v-container>
+      </v-layout>
+    </v-app>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: 'App',
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data() {
+    return {
+      dialog: false,
+      nav: [
+        {
+          icon: 'home',
+          text: 'Home',
+          title: 'Back to Home page',
+          active: true,
+          link: '/',
+        },
+        {
+          icon: 'info',
+          text: 'About',
+          title: 'About this demo',
+          active: false,
+          link: '/about',
+        },
+      ],
+    };
+  },
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+
+    v
+    <h1>This is an about page</h1>
+
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'About',
+  components: {},
+};
+</script>
