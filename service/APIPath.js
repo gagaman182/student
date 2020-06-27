b539e94ca23d43048433a6b5c802250c60ec5146
gagaman182 <csage182*>
2020-06-27 23:00:53 +0700

    edit  part 4.3 api

--- service/APIPath.js
+++ service/APIPath.js
@@ -1,5 +1,6 @@
 //const API_URL = 'http://192.168.5.187/0161/student/screen/api/';
-const API_URL = "http://localhost/student/api/";
+//const API_URL = "http://localhost/student/api/";
+const API_URL = 'http://192.168.4.3/webapp/tee/student/api/';
 //const API_URL = "http://192.168.31.165/student/api/";
 //const API_URL = "http://192.168.0.103/student/api/";
 
